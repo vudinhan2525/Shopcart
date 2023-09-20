eb6cac541f7ca45c222637574563e5f9fb2d8b98
vudinhan2525 <vudinhan000@gmail.com>
2023-09-20 16:21:37 +0700

    ui navbar

--- front-end/src/components/HeaderComponent/HeaderComponent.js
+++ front-end/src/components/HeaderComponent/HeaderComponent.js
@@ -1,23 +1,33 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo';
-import { UserIcon, CartIcon } from '../../utils/IconSVG/index';
+import { UserIcon, CartIcon, SearchIcon } from '../../utils/IconSVG/index';
 function HeaderComponent() {
   return (
     <div className=" flex py-3 items-center">
       <Link to="/" className="basis-1/4">
         {logo()}
       </Link>
-      <div className="basis-2/4">
-        <input></input>
+      <div className="basis-2/4 ">
+        <div className="w-[500px] mx-auto flex bg-gray-100 rounded-full">
+          <input
+            type="text"
+            placeholder="Tìm kiếm sản phẩm"
+            className=" outline-0 py-3 px-6  w-full bg-gray-100 rounded-full"
+            spellCheck="false"
+          ></input>
+          <div className="flex items-center py-3 px-4 cursor-pointer rounded-r-full text-[#a7a7ab] hover:text-black  hover:bg-[#e4e4e6]">
+            {SearchIcon()}
+          </div>
+        </div>
       </div>
       <ul className="flex basis-1/4 gap-5">
         <li className="flex gap-2 cursor-pointer p-3 rounded-full hover:bg-gray-100">
           {UserIcon()}
-          <p>Account</p>
+          <p>Tài khoản</p>
         </li>
         <li className="flex gap-2 cursor-pointer p-3 rounded-full hover:bg-gray-100">
           {CartIcon()}
-          <p>Cart</p>
+          <p>Giỏ Hàng</p>
         </li>
       </ul>
     </div>
--- front-end/src/utils/IconSVG/index.js
+++ front-end/src/utils/IconSVG/index.js
@@ -36,3 +36,22 @@ export const CartIcon = (width = '24px', height = '24px') => {
     </svg>
   );
 };
+export const SearchIcon = (width = '24px', height = '24px') => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      width={width}
+      height={height}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  );
+};
