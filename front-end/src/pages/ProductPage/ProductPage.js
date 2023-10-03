70a792b483aad456cc78e05f4c3ed1d4965e10b9
vudinhan2525 <vudinhan000@gmail.com>
2023-10-03 21:53:18 +0700

    nt

--- front-end/src/pages/ProductPage/ProductPage.js
+++ front-end/src/pages/ProductPage/ProductPage.js
@@ -1,15 +1,45 @@
 import Slider from 'react-slick';
+import { useState } from 'react';
 import img1 from '../../assets/img/cart/test.webp';
 import img2 from '../../assets/img/cart/test2.webp';
 import img3 from '../../assets/img/cart/test3.webp';
 import img4 from '../../assets/img/cart/test4.webp';
 import img5 from '../../assets/img/cart/test5.webp';
-const img = [img1, img2, img3, img4, img5];
+import LeftArrow from '../../assets/img/slider/ArrowLeft.svg';
+import RightArrow from '../../assets/img/slider/ArrowRight.svg';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+const img = [img1, img2, img3, img4, img5, img1, img2, img3, img4, img5];
 function ProductPage() {
-  const settings = {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const slider2Ref = useRef();
+  const slider1Ref = useRef();
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => <img src={LeftArrow} alt="prevArrow" {...props} />;
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    <img src={RightArrow} alt="nextArrow" {...props} />
+  );
+  useEffect(() => {
+    if (activeSlide - 2 <= 0) {
+      slider2Ref.current.slickGoTo(img.length + activeSlide - 2);
+    } else slider2Ref.current.slickGoTo(activeSlide - 2);
+    slider1Ref.current.slickGoTo(activeSlide);
+  }, [activeSlide]);
+  const settings1 = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SlickArrowRight />,
+    prevArrow: <SlickArrowLeft />,
+    beforeChange: (current, next) => {
+      setActiveSlide(next);
+    },
+  };
+  const settings2 = {
+    infinite: true,
+    focusOnSelect: false,
+    speed: 500,
+    slidesToShow: 5,
     slidesToScroll: 1,
   };
   return (
@@ -17,14 +47,31 @@ function ProductPage() {
       <p className="mt-5 mb-8 text-sm font-semibold">Home / SmartPhones / Iphone13</p>
       <div className="flex gap-16  ">
         <div className="basis-1/2">
-          <div className="w-[600px] h-[450px]">
-            <Slider {...settings}>
+          <div className="w-[450px] h-[400px] mx-auto">
+            <Slider {...settings1} ref={slider1Ref}>
               {img.map((el, idx) => {
                 return (
                   <div key={idx}>
                     <div
                       style={{ backgroundImage: `url(${el})` }}
-                      className="h-[450px] w-[450px] mx-auto bg-no-repeat bg-center bg-cover"
+                      className="h-[400px] w-[400px] mx-auto bg-no-repeat bg-center bg-cover"
+                    ></div>
+                  </div>
+                );
+              })}
+            </Slider>
+          </div>
+          <div className="w-[550px] h-[200px] mx-auto mt-5">
+            <Slider {...settings2} ref={slider2Ref}>
+              {img.map((el, idx) => {
+                return (
+                  <div key={idx} className={idx === activeSlide ? `border-primary-color border-2 rounded-sm` : ``}>
+                    <div
+                      style={{ backgroundImage: `url(${el})` }}
+                      onClick={() => {
+                        setActiveSlide(idx);
+                      }}
+                      className="h-[90px] w-[90px] my-[5px] mx-auto bg-no-repeat bg-center bg-cover "
                     ></div>
                   </div>
                 );
@@ -32,7 +79,9 @@ function ProductPage() {
             </Slider>
           </div>
         </div>
-        <div className="basis-1/2"></div>
+        <div className="basis-1/2">
+          <div className="bg-red-700 w-10 h-10 cursor-pointer" onClick={() => {}}></div>
+        </div>
       </div>
     </div>
   );
